d1a39e0854b28b6c581175013376676692639e3a
Alexandre Feliciano <alexandre.feliciano.t0131684@edu.atec.pt>
2026-02-19 17:47:25 +0000

    Add arithmetic, decision, and logical operators examples in operadores.js

---
+++ JS/operadores.js
@@ -0,0 +1,28 @@
+//operadores aritméticos
+let valor1 = 12
+let valor2 = 10
+let soma;
+
+// + soma, - subtração, / divisão, * multiplicação, % resto de divisão e ** exponencial
+soma=valor1+valor2
+console.log(valor1+valor2)
+console.log(valor1-valor2)
+console.log(valor1/valor2)
+console.log(valor1*valor2)
+console.log(valor1%valor2)
+console.log(valor1**valor2)
+
+// = atribuição, += , -= , *= , /=
+//soma+=valor1   ==   soma=soma+valor
+
+//operadores de decisão
+//  == se valores sao iguais
+// === se valores e tipo sao iguais
+// != se os valores sao diferentes
+// !== se valores e tipos sao diferentes (exemplo: 10 !== "10" true) 
+
+// < , > , <= , >=
+
+
+//operadores lógicos
+// && , || , !
