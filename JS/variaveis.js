072edc1c40af1109c0135d9866eca8c50a5dfe82
Alexandre Feliciano <alexandre.feliciano.t0131684@edu.atec.pt>
2026-02-19 17:19:11 +0000

    Add initial variable declarations and logging in variaveis.js

---
+++ JS/variaveis.js
@@ -0,0 +1,51 @@
+//Declaração e inicialização de variáveis
+
+//string
+let nome = "Alex"
+console.log(nome)
+console.log(typeof nome)
+
+//number
+let idade = 26
+let media = 12.4
+console.log(idade);
+console.log(typeof idade);
+console.log(media);
+console.log(typeof media);
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+//boolean
+let ativo = true;
+console.log(ativo);
+console.log(typeof ativo);
+
+//undefined
+let valor;
+console.log(valor);
+console.log(typeof valor);
+
+//null
+let nulo = null;
+console.log(nulo);
+console.log(typeof nulo); 
+
+//arrays
+
+let obje = ["BMW", "FIAT", "FORD"];
+console.log(obje);
+console.log(typeof obje); 
+
+//tipos de objetos
+let objec = {"BMW": 320,
+              "FIAT": "PUNTO",
+              "FORD":"RANGER"};
+
+
+console.log(objec);
+//acesso feito atraves de mapping, key : value
+console.log(objec["FIAT"]);
+console.log(typeof objec); 
+
