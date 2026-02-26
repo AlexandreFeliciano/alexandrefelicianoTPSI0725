dc2c07a437f02dd47483ed37683385ef8676f076
Alexandre Feliciano <alexandre.feliciano.t0131684@edu.atec.pt>
2026-02-26 15:42:15 +0000

    feat: add initial exercises for day classification and grade evaluation

---
+++ JS/funcoes_exercicios/Exercicios_Switch.js
@@ -0,0 +1,40 @@
+/*
+1. Tipo de dia
+Cria um programa que receba o nome de um dia da semana e diga se é dia útil ou fim de semana.
+Exemplo:
+Entrada → domingo
+Saída → Fim de semana
+*/
+
+/*
+const prompt = require("prompt-sync")();
+
+let dia;
+
+dia = prompt("Digite o nome de um dia da semana: ");
+
+function verificarDia(dia) {
+
+    if (dia === "segunda-feira" || dia === "terça-feira" || dia === "quarta-feira" || dia === "quinta-feira" || dia === "sexta-feira") {
+        console.log(`A ${dia} é um dia útil`)
+    } else {
+        console.log(`O ${dia} é um dia de fim de semana`)
+    }
+}
+
+verificarDia(dia);
+*/
+
+/*
+2. Classificação de nota
+Lê uma nota (0–100) e retorna uma classificação:
+•	90 ou mais → Excelente
+•	70–89 → Bom
+•	50–69 → Suficiente
+•	Abaixo de 50 → Insuficiente
+Exemplo:
+Entrada → Bom
+Saída →  70-89
+*/
+
+
